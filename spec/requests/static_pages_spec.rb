7c6fec68aa40a385972d6370040edf9389a40960
Jacob <jacobmollat@gmail.com>
2017-08-09 22:34:01 +0200

    Add Contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,7 +4,7 @@ describe "Static pages" do
 
  describe "Home page" do
 
-   it "should have the content 'Sample App'" do
+   it "should have the h1 'Sample App'" do
 
      visit '/static_pages/home'
      page.should have_selector('h1', :text => 'Sample App')
@@ -18,7 +18,7 @@ describe "Static pages" do
 
   describe "Help page" do
 
-    it "should have the content 'Help'" do
+    it "should have the h1 'Help'" do
     visit '/static_pages/help'
       page.should have_selector('h1', :text => 'Help')
     end
@@ -28,17 +28,31 @@ describe "Static pages" do
     end
    end
 
-  describe "About page" do
+  describe "Contact page" do
 
-    it "should have the content 'About Us'" do
+    it "should have the h1 'Contact'" do
 
-      visit '/static_pages/about'
-      page.should have_selector('h1', :text => 'About Us')
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
       end
 
       it "should have the right title" do
-        visit '/static_pages/about'
-        page.should have_title("Ruby on Rails Tutorial Sample App | About Us")
+        visit '/static_pages/contact'
+        page.should have_title("Ruby on Rails Tutorial Sample App | Contact")
       end
    end
+
+   describe "About page" do
+
+     it "should have the h1 'About Us'" do
+
+       visit '/static_pages/about'
+       page.should have_selector('h1', :text => 'About Us')
+       end
+
+       it "should have the right title" do
+         visit '/static_pages/about'
+         page.should have_title("Ruby on Rails Tutorial Sample App | About Us")
+       end
+    end
 end
